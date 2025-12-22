0bb21e8e9dfe12c3c637289dbb65df38cfc52406
guru <incredibleguru1004@gmail.com>
2025-12-21 23:49:12 -0500

    typescript eslint

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,18 +1,54 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
-
+import { defineConfig } from 'eslint/config'
+import eslintPlugin from '@eslint/js'
+import { configs as tseslintConfigs } from 'typescript-eslint'
+ 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
-
-export default eslintConfig;
+    {
+        name: 'project/javascript-recommended',
+        files: ['**/*.{js,mjs,ts,tsx}'],
+        ...eslintPlugin.configs.recommended,
+    },
+])
+ 
+const typescriptConfig = defineConfig([
+    {
+        name: 'project/typescript-strict',
+        files: ['**/*.{ts,tsx,mjs}'],
+        extends: [
+            ...tseslintConfigs.strictTypeChecked,
+            ...tseslintConfigs.stylisticTypeChecked,
+        ],
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+                ecmaFeatures: {
+                    jsx: true,
+                },
+                warnOnUnsupportedTypeScriptVersion: true,
+            },
+        },
+        rules: {
+            '@typescript-eslint/no-unsafe-call': 'off',
+            '@typescript-eslint/triple-slash-reference': 'off',
+        },
+    },
+    {
+        name: 'project/javascript-disable-type-check',
+        files: ['**/*.{js,mjs,cjs}'],
+        ...tseslintConfigs.disableTypeChecked,
+    }
+])
+ 
+const ignoresConfig = defineConfig([
+    {
+        name: 'project/ignores',
+        ignores: ['.next/', 'node_modules/', 'public/', '.vscode/', 'next-env.d.ts']
+    },
+])
+ 
+export default defineConfig([
+    ...ignoresConfig,
+    ...eslintConfig,
+    ...typescriptConfig,
+])
